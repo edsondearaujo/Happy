2f3d025a5661ad7f80d9b5577d10ec2b92adea52
Edson Soares <39536164+edsondearaujo@users.noreply.github.com>
2020-10-16 01:19:40 -0300

    Revert "map"

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
-
-import  'leaflet/dist/leaflet.css';
+import { Link } from 'react-router-dom'
+import { FiPlus } from 'react-icons/fi'
 
 import mapMarkerImg from '../image/map-marker.svg'
-
-import '../style/pages/orphanages-map.css';
 
 function OrphanagesMap() {
     return(
@@ -26,15 +21,7 @@ function OrphanagesMap() {
                 </footer>
             </aside>
 
-            <Map
-                center={[-15.8280187,-47.920746]}
-                zoom={12.12}
-                style={{width: '100%', height: '100%'}}
-            >
-                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            </Map>
-
-
+            <div></div>
             <Link to="" className="create-orphanage">
                 <FiPlus size={32} color="#FFF" />
             </Link>
